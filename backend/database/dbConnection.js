27e0d64830a9e2545e5a48b5dc8d2b23869010e1
sakshi-prgm <sakshichoudhary22042004@gmail.com>
2025-10-24 12:29:54 +0530

    Initial commit: full-stack project

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,26 @@
+// import mongoose from "mongoose";
+
+// export const dbConnection = () => {
+//   mongoose
+//     .connect(process.env.MONGO_URI, {
+//       dbName: "RESERVATIONS",
+//     })
+//     .then(() => {
+//       console.log("Connected to database!");
+//     })
+//     .catch((err) => {
+//       console.log(`Some error occured while connecing to database: ${err}`);
+//     });
+// };
+
+import mongoose from "mongoose";
+
+export const dbConnection=async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("mongodb connected successfully");
+    }
+    catch(error){
+        console.log(error);
+    }
+};
